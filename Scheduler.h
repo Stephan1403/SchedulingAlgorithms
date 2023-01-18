a4c064ecb9e67831593432fa7f4600324bf7ebb1
Stephan <Stephanf2003>
2023-01-18 23:46:33 +0100

    introduced job_counter

--- Scheduler.h
+++ Scheduler.h
@@ -15,16 +15,21 @@ class Scheduler{
             this->p_vector = p_vector;
             this->method_id = method_id;
 
+            this->current_job = nullptr;
             this->current_time = 0;
+            this->job_counter = 0; 
         }
 
         // Returns the next available job / process assigned by the Scheduler
         Process* next_job(){
 
             // Check if last job needs more time
-            if(current_job != nullptr)
+            if(current_job != nullptr){
                 if (!(*current_job).is_completed())
                     ready_processes.push_back(current_job);     // Process not done yet
+                else 
+                    job_counter++;
+            }
 
             if(p_queue.empty())                                 // Set to nullptr if none available
                 current_job = nullptr;
@@ -35,9 +40,9 @@ class Scheduler{
         }
 
 
-
         // Look for new ready processes and add them to the ready queue
-        void update_p_queue(){
+        void update_p_queue(int time){
+            this->current_time = time;                                  // Update time
             update_ready_processes();
 
             // Move queue processes to ready_processes
@@ -65,6 +70,9 @@ class Scheduler{
         }
 
 
+        int get_job_counter(){ return this->job_counter;}
+
+
 
     private:
         // Check for any ready processes inside p_vector and add them to ready_processes
@@ -73,7 +81,7 @@ class Scheduler{
 
             for(auto pro: this->p_vector){
 
-                if(!pro->is_scheduled() && pro->get_r_time() >= current_time){
+                if(!pro->is_scheduled() && pro->get_r_time() <= current_time){
                     // Add the ready process
                     ready_processes.push_back(pro);
                     pro->set_scheduled(); 
@@ -86,6 +94,8 @@ class Scheduler{
         
         int current_time;                                   // Current time to determine the ready processes
         int method_id;                                      // Method used to schedule processes
+
+        int job_counter;                                    // Number of process finished
 
         Process* current_job;                               // The Process that is currently running
         std::vector<Process*> p_vector;                     // Stores all processes
